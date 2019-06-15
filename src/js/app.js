f99b6c145bab5432e00d0da5bf7269e13a2d7242
AngelikaD <37298658+AngelikaD@users.noreply.github.com>
2019-06-15 15:48:46 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ var title = document.querySelector('.title');
 var courseFeatureElements = document.querySelectorAll('.course-feature');
 var button = document.querySelector('button');
 
-navigator.serviceWorker.register('/mobweb/cba/public/sw.js');
+navigator.serviceWorker.register('/sw.js');
 
 function animate() {
   title.classList.remove('animate-in');
